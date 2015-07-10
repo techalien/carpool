d2f7e1d18854c779746643482ec77831e1b7e9d8
techalien <akhil@techalien.in>
2015-07-10 18:37:59 +0530

    debug

--- public/controllers/form.js
+++ public/controllers/form.js
@@ -1,13 +1,15 @@
 angular.module('carpooler')
   .controller('AddCtrl', function($scope, $alert, Travel) {
-    $scope.addEvent = function() {
-      Travel.save({ Name: $scope.Name,
+    $scope.addTravel = function() {
+      Travel.save({
+        Name: $scope.Name,
         userEmail:$scope.userEmail,
         phoneNum:$scope.phoneNum,
         Source:$scope.Source,
         Destination:$scope.Destination,
         travelDate:$scope.travelDate,
-        travelTime:$scope.travelTime}).$promise
+        travelTime:$scope.travelTime
+        }).$promise
         .then(function() {
           $scope.Name = '';
           $scope.phoneNum = '';
@@ -25,7 +27,7 @@ angular.module('carpooler')
           });
         })
         .catch(function(response) {
-          $scope.Destination = '';
+          $scope.Destination = 'fail';
           $scope.addForm.$setPristine();
           $alert({
             content: response.data.message,
