926bf4805c0027f0875e4139ebc5f58f00bc631b
Karan Sharma <karansharma1295@gmail.com>
2015-07-10 22:56:25 +0530

    wireup

--- public/controllers/detail.js
+++ public/controllers/detail.js
@@ -1,12 +1,14 @@
 angular.module('carpooler')
-  .controller('MainCtrl', function($scope, Travel) {
-    $scope.headingTitle = 'Songs in Library';
-    $scope.shows = Travel.query();
-    $scope.filterBySource = function(genre) {
+  .controller('DetailCtrl', function($scope, Travel) {
+    $scope.headingTitle = 'Fellow Cab Goers';
+    $scope.sources=; // get list of sources from Mongoose
+    $scope.destinations=;// get list of destinations from Mongoose
+    $scope.travels = Travel.query();        //Run a search query within Angular of Source
+    $scope.filterBySource = function(sources) {
       $scope.travels = Travel.query({ source: source });
       $scope.headingtitle = source;
     };
-    $scope.filterByDestination = function(char) {
+    $scope.filterByDestination = function(destinations) {       //Run a search query within Angular of Destination
       $scope.travels = Travel.query({ destination: destination });
       $scope.headingTitle = destination;
     };
