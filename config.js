384705f789809166de6153b898a46ae5030c80a2
Karan Sharma <karansharma1295@gmail.com>
2015-07-14 01:31:35 +0530

    mongo-reset

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   //TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
   //MONGO_URI: process.env.MONGO_URI || 'localhost',
   TOKEN_SECRET: 'thisispasswordforcarpoolapp',
-  MONGO_URI: 'mongodb://karan:breeze@ds047732.mongolab.com:47732/breeze'
+  MONGO_URI: 'mongodb://carpooler:ilikecarpooling@ds053312.mongolab.com:53312/carpooler'
 };
 
 
