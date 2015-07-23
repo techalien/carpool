66ab2985c212342ade1ca905624287cf38a65088
techalien <akhil@techalien.in>
2015-07-24 00:00:13 +0530

    fucked up api

--- public/controllers/home.js
+++ public/controllers/home.js
@@ -49,11 +49,11 @@ angular.module('carpooler')
                     console.log('Error: ' + data);
                 });
         };
-        var distance =[] ;
+        var distance =[];
         var directionsService = new google.maps.DirectionsService();
-        function calcRoute(p1,p2,callback) {
-          var start = p1;
-          var end = p2;
+        function calcRoute(place1,place2,callback) {
+          var start = place1;
+          var end = place2;
           var args = {
               origin:start,
               destination:end,
@@ -79,7 +79,7 @@ angular.module('carpooler')
               data.travelTime = new moment(data.travelTime).format("h:mm:ss a");
               $scope.bookingReference = data;
               for(var i = 0;i<$scope.bookings.length;i++) {
-                calcRoute($scope.bookings[i].Destination,$scope.bookingReference.Destination,callback(dist){
+               calcRoute($scope.bookings[i].Destination,$scope.bookingReference.Destination,function(dist){
                   distance.push(dist);
                 });
               }
